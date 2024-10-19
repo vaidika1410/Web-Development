75b053f3dafc1600e38a516b58d3951bf67945aa
vaidika1410 <vaidikakaul20@gmail.com>
2024-10-19 04:33:14 +0000

    Learning TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], 
+  // in the src folder, this file will monitor every folder and subfolders with the extension of .html and .js
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
