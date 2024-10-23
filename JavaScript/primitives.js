7a88bfbb715c83884434c9f91b8e490d3ee08d86
vaidika1410 <vaidikakaul20@gmail.com>
2024-10-23 15:00:01 +0000

    Primitives in JS

--- JavaScript/primitives.js
+++ JavaScript/primitives.js
@@ -1,5 +1,42 @@
 //number
 
 let balance = 120
+let newBalance = new Number(120)
 
 console.log(typeof(balance))
+console.log(typeof(newBalance)) 
+
+// null and undefined
+
+let firstname;
+let lastname = null;
+let user = undefined
+
+console.log(firstname);
+console.log(lastname);
+console.log(user);
+
+//string
+
+let myString = "hello"
+let myString1 = 'Bonjour'
+let username = `vaidika`
+
+let oldGreet = myString + " " + username;
+console.log(oldGreet)
+
+let newGreet = `Hello ${username}`
+console.log(newGreet);
+
+
+//symbol
+let sm1 = Symbol();
+let sm2 = Symbol();
+
+console.log(sm1 == sm2);
+
+let newSymbol = Symbol("vaidika")
+let newSymbol1 = Symbol("vaidika")
+console.log(newSymbol == newSymbol1);
+
+// the main feature of symbol is that it provides a unique value
