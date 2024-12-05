c1dd050d5c362100cb88b77fc70def235d75bd39
vaidika1410 <vaidikakaul20@gmail.com>
2024-12-05 06:27:18 +0000

    Challenges on Conditions

---
+++ JavaScript/Conditions/check4.js
@@ -0,0 +1,9 @@
+//checking if a boolean value is true or false:
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("yes user is logged in");
+} else {
+    console.log("no is logged out");
+}
---
+++ JavaScript/Conditions/check5.js
@@ -0,0 +1,11 @@
+//checking if an array is empty or not:
+
+let items = []
+
+console.log(items.length)
+
+if (items.length === 0) {
+    console.log("array is empty")
+} else {
+    console.log("array is NOT empty")
+}
