e957b2ce4b2956792f3eb2f3f9aef0c379bfb3a4
vaidika1410 <vaidikakaul20@gmail.com>
2025-04-03 18:33:03 +0000

    Generators, Iterators and Import and Export of modules in ES6 and common JS

---
+++ JavaScript/AdvancedJS/exportImportOfModules/appM.js
@@ -0,0 +1,10 @@
+// module import file
+
+// default import
+import multiply from "./mathOperationsM.js";
+
+// named import
+import {add, substract} from "./mathOperationsM.js";
+
+console.log(multiply(2,3));
+console.log(add(2,2));
