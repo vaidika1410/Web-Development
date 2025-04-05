54a55772c09209ab85cf750b0b3428b2c40019b9
vaidika1410 <vaidikakaul20@gmail.com>
2025-04-05 12:40:47 +0000

    To-do list project

--- Projects/project1:Todo-list/script.js
+++ Projects/project1:Todo-list/script.js
@@ -1,22 +1,47 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskButton = document.getElementById("add-task-button");
-const todoList = document.getElementById("todo-list");
+document.addEventListener("DOMContentLoaded", () => {
+    const todoInput = document.getElementById("todo-input");
+    const addTaskButton = document.getElementById("add-task-button");
+    const todoList = document.getElementById("todo-list");
 
 
-let tasks = []
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-addTaskButton.addEventListener("click", function(){
-    const taskText = todoInput.value.trim();
+    tasks.forEach((task) => renderTask(task));
 
-    if(taskText === "") return;
+    addTaskButton.addEventListener("click", function () {
+        const taskText = todoInput.value.trim();
 
-    const newTask = {
-        id : Date.now(),
-        text : taskText,
-        completed : false
+        if (taskText === "") return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask);
+        saveTasks();
+        todoInput.value = ""; //clears input field
+        console.log(tasks);
+    })
+
+    //render tasks individually
+    function renderTask(task) {
+        // console.log(task.text)
+
+        const li = document.createElement("li");
+        li.setAttribute("data-id", task.id);
+        li.innerHTML = `
+            <span>
+                ${task.text}
+            </span>
+            <button>Delete</button>
+            `
+        todoList.appendChild(li);
     }
 
-    tasks.push(newTask);
-    todoInput.value = ""; //clears input field
-    console.log(tasks);
+
+    function saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }
 })
