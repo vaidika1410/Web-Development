52c179e57fb653bafb4b18f582442f5fb8af71ac
vaidika1410 <vaidikakaul20@gmail.com>
2025-05-24 12:39:52 +0000

    To-Do list

---
+++ JavaScript/TodoList/todoList.js
@@ -0,0 +1,60 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.getElementById("todo-input")
+    const addTaskButton = document.getElementById("add-task-btn")
+    const todoList = document.getElementById("todo-list")
+
+    let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+    tasks.forEach((task) => {
+        renderTasks(task)
+    })
+
+    addTaskButton.addEventListener('click', function () {
+        const taskText = todoInput.value.trim()
+        if (taskText === "") return;
+
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask)
+        renderTasks(newTask);
+        saveTasks();
+        todoInput.value = "" //clears the input
+        console.log(tasks)
+    })
+
+    function renderTasks(task) {
+        const li = document.createElement("li")
+        li.setAttribute('data-id', task.id)
+        if(task.completed) li.classList.add("completed")
+        li.innerHTML = `
+            <span>${task.text}</span>
+            <button>delete</button>
+        `;
+        li.addEventListener("click", function(e){
+            if(e.target.tagName === 'BUTTON') return;
+
+            task.completed = !task.completed
+
+            li.classList.toggle("completed")
+            saveTasks();
+        })
+
+        li.querySelector('button').addEventListener('click', (e) =>{
+            e.stopPropagation() //prevent toggle from firing
+            tasks = tasks.filter(t => t.id != task.id)
+            li.remove();
+            saveTasks();
+        })
+
+        todoList.appendChild(li)
+    }
+
+
+    function saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+})
