8bf3d84bb5012f044d012d9aca3265e777426e44
Vaidika Kaul <vaidikakaul20@gmail.com>
2025-07-30 10:34:55 +0000

    Correct Content type

--- nodeJS/Nginx-server/server.js
+++ nodeJS/Nginx-server/server.js
@@ -23,11 +23,11 @@ const server = http.createServer((req, res) => {
     fs.readFile(filePath, (error, content) => {
         if(error) {
             if(error.code === 'ENOENT'){
-                res.writeHead(404, {'content-type': 'text/html'})
+                res.writeHead(404, {'Content-Type': 'text/html'})
                 res.end('file not found')
             }
         } else {
-            res.writeHead(200, {'content-type': contentType})
+            res.writeHead(200, {'Content-Type': contentType})
             res.end(content, 'utf-8')
         }
     })
